1e6c1c7e06cf72f98d7cf94b1f24bd0f01f2b046
Andrei Dumitrescu <5057797+andreidmt@users.noreply.github.com>
2022-04-17 22:53:26 +0200

    feat: add new "node-esm" target

---
+++ targets/node-esm.js
@@ -0,0 +1,33 @@
+/* eslint-env node */
+
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+  },
+
+  env: {
+    node: true,
+  },
+
+  extends: [
+    ...[
+      "../rules/json",
+      "../rules/best.practices",
+      "../rules/comments",
+      "../rules/errors",
+      "../rules/es6",
+      "../rules/jsdoc",
+      "../rules/node",
+      "../rules/promise",
+      "../rules/style",
+      "../rules/unicorn",
+      "../rules/variables",
+      "../rules/prettier",
+    ].map(require.resolve),
+  ],
+
+  plugins: ["no-inferred-method-name"],
+
+  settings: {},
+}
