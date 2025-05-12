6175678b04c52c48a1df7aa778af0cfaf9532add
Andrei Dumitrescu <5057797+andreidmt@users.noreply.github.com>
2025-05-12 19:16:57 +0200

    style: add eslint-import rules to own lint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,10 @@ import prettierConfig from "eslint-plugin-prettier/recommended"
 import globals from "globals"
 
 import baseConfig from "./src/rules/base.js"
+import importConfig from "./src/rules/import.js"
 import jsdocConfig from "./src/rules/jsdoc.js"
-import unicornConfig from "./src/rules/unicorn.js"
 import tsConfig from "./src/rules/typescript.js"
+import unicornConfig from "./src/rules/unicorn.js"
 
 /** @satisfies {import("eslint").Linter.Config} */
 export default /** @type {const} */ ({
@@ -26,12 +27,17 @@ export default /** @type {const} */ ({
   plugins: {
     ...unicornConfig.plugins,
     ...jsdocConfig.plugins,
+    ...importConfig.plugins,
     ...prettierConfig.plugins,
   },
   rules: {
     ...baseConfig.rules,
     ...unicornConfig.rules,
     ...jsdocConfig.rules,
+    ...importConfig.rules,
     ...prettierConfig.rules,
+  },
+  settings: {
+    ...importConfig.settings,
   },
 })
